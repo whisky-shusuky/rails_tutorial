598e88f140c66bfd35114143e041a17012870ea6
whisky-shunsuke <whisky.shusuky@gmail.com>
2018-08-28 23:06:13 +0900

    Add Static Pages.(3.2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/buz'
+  get 'static_pages/home'
+  get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
 end
