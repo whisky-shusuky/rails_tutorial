545b0725eacb3aef882ba8584b17d4a0f79d8bd8
whisky-shunsuke <whisky.shusuky@gmail.com>
2018-09-04 20:55:04 +0900

    Add spec test.

--- spec/requests/projects_apis_spec.rb
+++ spec/requests/projects_apis_spec.rb
@@ -1,30 +1,57 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe "ProjectsApis", type: :request do
   describe "GET /projects_apis" do
-    it "connects Sample App page" do
-      get '/static_pages/home'
+
+    let(:base_title) { 'Ruby on Rails Tutorial Sample' }
+
+    it "connects Sample App pages." do
+#      get '/static_pages/home'
+      get root_path
       expect(response).to have_http_status(200)
     end
 
-    it "connects Sample App page" do
-      get '/static_pages/help'
+    it "connects Help pages." do
+#      get '/static_pages/help'
+      get help_path
       expect(response).to have_http_status(200)
     end
 
-    it "connects Sample App page" do
-      get '/static_pages/about'
+    it "connects About pages." do
+#      get '/static_pages/about'
+      get about_path
       expect(response).to have_http_status(200)
     end
 
-    it "connects Contact" do
-      get '/static_pages/contact'
+    it "connects Contact pages." do
+#      get '/static_pages/contact'
+      get contact_path
       expect(response).to have_http_status(200)
     end
 
-    it "connects Sample App page" do
-      get '/'
+    it "has Home title." do
+      get root_path
       expect(response).to have_http_status(200)
+      assert_select "title", "#{base_title}"
+    end
+
+    it "has Help title." do
+      get help_path
+      expect(response).to have_http_status(200)
+      assert_select "title", "Help | #{base_title}"
+    end
+ 
+    it "has About title." do
+      get about_path
+      expect(response).to have_http_status(200)
+      assert_select "title", "About | #{base_title}"
+    end
+
+    it "has Contact title." do
+      get contact_path
+      expect(response).to have_http_status(200)
+      assert_select "title", "Contact | #{base_title}"
     end
 
 ### original test
@@ -33,7 +60,8 @@ RSpec.describe "ProjectsApis", type: :request do
     end
 
     it "responds with text/html formated output" do
-      get '/static_pages/home'
+#      get '/static_pages/home'
+      get root_path
       expect(response.content_type).to eq "text/html"
     end
   end
